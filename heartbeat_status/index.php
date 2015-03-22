9d58b529b7dd04fc7682c05ebf669f1954ad9164
wjsite <wjsite@yahoo.com>
2015-03-22 19:55:47 +0800

    update

--- heartbeat_status/index.php
+++ heartbeat_status/index.php
@@ -25,62 +25,62 @@ $heartbeat_list=get_heartbeat_list();
 </div>
 
 <div class="div_block">
-	<table>
-		<tr class="table-head">
-			<th>Seq.</th>
-			<th>Host</th>
+  <table>
+    <tr class="table-head">
+      <th>Seq.</th>
+      <th>Host</th>
       <th>Service</th>
-			<th>Component</th>
-			<th>Beat Interval</th>
-			<th>Beat Time</th>
-			<th>Duration</th>
-		</tr>
-		<?php
-		$i=1;
-		foreach($heartbeat_list as $item){
-		  if(!$item['status']){ ?>
-		<tr class="<?php echo $i%2==0?'even-color':''; ?>">
-			<td><?php echo $i++; ?></td>
-			<td><?php echo $item['host']; ?></td>
+      <th>Component</th>
+      <th>Beat Interval</th>
+      <th>Beat Time</th>
+      <th>Duration</th>
+    </tr>
+    <?php
+    $i=1;
+    foreach($heartbeat_list as $item){
+      if(!$item['status']){ ?>
+    <tr class="<?php echo $i%2==0?'even-color':''; ?>">
+      <td><?php echo $i++; ?></td>
+      <td><?php echo $item['host']; ?></td>
       <td><?php echo $item['service']; ?></td>
-			<td><?php echo $item['component']; ?></td>
-			<td><?php echo $item['beat_interval']; ?>s</td>
-			<td>
+      <td><?php echo $item['component']; ?></td>
+      <td><?php echo $item['beat_interval']; ?>s</td>
+      <td>
         <span style="color:#E06043;">
           <?php echo $item['beat_time'];?>
         <span>
       </td>
       <td><?php echo $item['duration'];?>m</td>
-		</tr>
-		<?php }} ?>
-	</table>
+    </tr>
+    <?php }} ?>
+  </table>
 </div>
 
 <div class="div_block">
-	<table>
-		<tr class="table-head">
-			<th>Seq.</th>
-			<th>Host</th>
+  <table>
+    <tr class="table-head">
+      <th>Seq.</th>
+      <th>Host</th>
       <th>Service</th>
-			<th>Component</th>
-			<th>Beat Interval</th>
-			<th>Beat Time</th>
-		</tr>
-		<?php $i=1;foreach($heartbeat_list as $item){if($item['status']){ ?>
-		<tr class="<?php echo $i%2==0?'even-color':''; ?>">
-			<td><?php echo $i++; ?></td>
-			<td><?php echo $item['host']; ?></td>
+      <th>Component</th>
+      <th>Beat Interval</th>
+      <th>Beat Time</th>
+    </tr>
+    <?php $i=1;foreach($heartbeat_list as $item){if($item['status']){ ?>
+    <tr class="<?php echo $i%2==0?'even-color':''; ?>">
+      <td><?php echo $i++; ?></td>
+      <td><?php echo $item['host']; ?></td>
       <td><?php echo $item['service']; ?></td>
-			<td><?php echo $item['component']; ?></td>
-			<td><?php echo $item['beat_interval']; ?>s</td>
-			<td>
+      <td><?php echo $item['component']; ?></td>
+      <td><?php echo $item['beat_interval']; ?>s</td>
+      <td>
         <span style="color:#095720;">
           <?php echo $item['beat_time'];?>
         <span>
       </td>
-		</tr>
-		<?php }} ?>
-	</table>
+    </tr>
+    <?php }} ?>
+  </table>
 </div>
 
 </body>
